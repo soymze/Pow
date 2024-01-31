e74fad5d4a3af8abb354b7cd5ee8f1bbc2c4d5ac
soymze <soylmze@gmail.com>
2024-01-31 21:44:32 +0300

    as

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class Main {
+
+    static int pow(int a, int b){
+
+        if (b == 0) {
+            return 1;
+        } else {
+            return a * pow(a, b - 1);
+        }
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int taban,us;
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Taban değerini giriniz:");
+        taban = sc.nextInt();
+        System.out.println("Üs değerini giriniz:");
+        us = sc.nextInt();
+
+        System.out.println(pow(taban,us));
     }
 }
